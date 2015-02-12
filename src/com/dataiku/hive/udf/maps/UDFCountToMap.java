d928324bbcb733277df78131c6e573aa36542c05
willwest <wmw71190@gmail.com>
2015-02-12 17:29:29 +0000

    Updating CountToMap and UDFMapFilterTopN to work with Spark SQL

--- src/com/dataiku/hive/udf/maps/UDFCountToMap.java
+++ src/com/dataiku/hive/udf/maps/UDFCountToMap.java
@@ -1,13 +1,44 @@
 package com.dataiku.hive.udf.maps;
 
-import org.apache.hadoop.hive.ql.exec.UDF;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF.DeferredObject;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorFactory;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.ListObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorUtils;
+import org.apache.hadoop.hive.serde2.objectinspector.PrimitiveObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.JavaStringObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.JavaIntObjectInspector;
 
 import java.util.*;
 
-public class UDFCountToMap extends UDF {
+public class UDFCountToMap extends GenericUDF {
+    ListObjectInspector loi;
+    JavaStringObjectInspector elementOI;
 
+    public String getDisplayString(String[] args){
+	return "UDFCountToMap()";
+    }
 
-    public Map<String, Integer> evaluate(List<String> a) {
+    public ObjectInspector initialize(ObjectInspector[] arguments) throws UDFArgumentException{
+	if (arguments.length != 1){
+	    throw new UDFArgumentException("UDFCountToMap takes exactly 1 argument");
+	}
+
+	ObjectInspector a = arguments[0];
+	this.loi = (ListObjectInspector) a;
+
+	JavaStringObjectInspector soi = PrimitiveObjectInspectorFactory.javaStringObjectInspector;
+	JavaIntObjectInspector ioi = PrimitiveObjectInspectorFactory.javaIntObjectInspector;
+	return ObjectInspectorFactory.getStandardMapObjectInspector(soi, ioi);
+    }
+
+    public Map<String, Integer> evaluate(GenericUDF.DeferredObject[] arguments) throws HiveException{
+	List<String> a = (List<String>) this.loi.getList(arguments[0].get());
+	
         HashMap<String, Integer> map= new HashMap<String, Integer>();
         if (a == null) {
             return null;
--- src/com/dataiku/hive/udf/maps/UDFMapValueFilterTopN.java
+++ src/com/dataiku/hive/udf/maps/UDFMapValueFilterTopN.java
@@ -2,13 +2,55 @@ package com.dataiku.hive.udf.maps;
 
 import org.apache.hadoop.hive.ql.exec.UDF;
 
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.hive.ql.metadata.HiveException;
+import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
+import org.apache.hadoop.hive.ql.udf.generic.GenericUDF.DeferredObject;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorFactory;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.ListObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.MapObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorUtils;
+import org.apache.hadoop.hive.serde2.objectinspector.PrimitiveObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.PrimitiveObjectInspectorFactory;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.JavaStringObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.JavaIntObjectInspector;
+import org.apache.hadoop.hive.serde2.objectinspector.primitive.WritableConstantIntObjectInspector;
+
 import java.util.*;
 
 /**
  * Filter topN Elements from a map
  */
-public class UDFMapValueFilterTopN extends UDF {
-    public Map<String, Integer> evaluate(Map<String, Integer> map, Integer n) {
+public class UDFMapValueFilterTopN extends GenericUDF {
+    MapObjectInspector moi;
+    JavaStringObjectInspector soi;
+    WritableConstantIntObjectInspector noi;
+    
+    public String getDisplayString(String[] args){
+        return "UDFMapFilterTopN()";
+    }
+
+    public ObjectInspector initialize(ObjectInspector[] arguments) throws UDFArgumentException{
+        if (arguments.length != 2){
+            throw new UDFArgumentException("UDFMapFilterTopN takes exactly 2 arguments");
+        }
+
+        ObjectInspector a = arguments[0];
+        this.moi = (MapObjectInspector) a;
+
+	ObjectInspector b = arguments[1];
+	this.noi = (WritableConstantIntObjectInspector) b;
+
+        JavaStringObjectInspector soi = PrimitiveObjectInspectorFactory.javaStringObjectInspector;
+        JavaIntObjectInspector ioi = PrimitiveObjectInspectorFactory.javaIntObjectInspector;
+        return ObjectInspectorFactory.getStandardMapObjectInspector(soi, ioi);
+    }
+
+    public Map<String, Integer> evaluate(GenericUDF.DeferredObject[] arguments) throws HiveException {
+	Map<String, Integer> map = (Map<String, Integer>) this.moi.getMap(arguments[0].get());
+	Integer n = ((IntWritable)arguments[1].get()).get();
 
         if (map.size() < n) {
             return map;
